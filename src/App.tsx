5bebb022a512d70374ebf49482da3568b1e38471
Davi @Senac <davi.frrs@gmail.com>
2023-09-13 21:41:01 -0300

    refactor: Removido MercadoPago

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import { RouterProvider } from "react-router-dom"
 import { AuthContextProvider } from "./contexts/AuthContext"
 import { router2 } from "./router"
-import { initMercadoPago } from "@mercadopago/sdk-react";
+import { loadStripe } from "@stripe/stripe-js"
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe('pk_test_51Nq3F7Ez6oa2bkpU2cKNj50NS4r0yd0BvXDUrZStk2kYesiwdbqp8Uc4iCDPjdeHP3NAQlEwPLDaZrFgqXNlrgGP009ak11IZe');
 
 function App() {
-  const mercadoPagoPK = import.meta.env.VITE_PAY_BROKER_PUBLIC_KEY;
-
-  console.log(mercadoPagoPK);
-
-  if (mercadoPagoPK) {
-    initMercadoPago(mercadoPagoPK);
+  const options = {
+    clientSecret: 'sk_test_51Nq3F7Ez6oa2bkpUYnGIZJaxzHec0LGk1jt9dMRljwK3kUzcrYyIqqDoJd9OE11b0h4b9CyX6QR546apBqc13BSY00EURk2Kvz'
   }
 
 
   return (
     <AuthContextProvider>
-      <RouterProvider router={router2} />
+      <Elements stripe={stripePromise} options={options}>
+        <RouterProvider router={router2} />
+      </Elements>
     </AuthContextProvider>
   )
 }
--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,62 +1,23 @@
 import { Button } from "@/components/ui/button";
 import { baseApi } from "@/lib/api";
-import { customization, initialization } from "@/lib/payment";
-import { Payment } from "@mercadopago/sdk-react";
+import { PaymentElement } from "@stripe/react-stripe-js";
 import { useState } from "react";
 
 
 export function Checkout() {
     const [isLoading, setIsLoading] = useState(true);
 
-    const onReadyBricks = async () => {
-        setIsLoading(false);
-    }
-
-    const onError = async (error) => {
-        // callback chamado para todos os casos de erro do Brick
-        console.log(error);
-        console.warn("Erro ao carregar gerenciador de pagamento...");
-        console.warn("Atualize a página")
-    };
-
-    const onSubmitPayment = async ({
-        selectedPaymentMethod, formData
-    }) => {
-        return new Promise(async (resolve, reject) => {
-
-            try {
-                const response = await baseApi.post("/pagamentos/process_payment", formData);
-
-                console.log(response.data)
-
-                resolve(response.data);
-            } catch (err) {
-                reject(err);
-            }
-        });
-    }
-
     return (
         <div>
             <h1>Checkout page!</h1>
 
-            {isLoading && <p>Carregando...</p>}
+            {/* {isLoading && <p>Carregando...</p>} */}
+            <form>
+                <PaymentElement />
 
-            <Payment
-                locale="pt-BR"
-                initialization={initialization}
-                customization={{
-                    paymentMethods: {
-                        creditCard: "all",
-                        debitCard: "all",
-                        ticket: "all",
-                    }
-                }}
-                onSubmit={onSubmitPayment}
-                onError={onError}
-                onReady={onReadyBricks}
-            />
-            {/* <Button onClick={test}>Teste</Button> */}
+                <Button type="submit" >Teste</Button>
+            </form>
+
         </div>
     )
 }
