dc92ac48e66e9c89e00ee556b37aab883e3cd2f8
Davi <davi.frrs@gmail.com>
2023-10-26 13:47:00 -0300

    feat: Implementado React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { RouterProvider } from "react-router-dom"
 import { AuthContextProvider } from "./contexts/AuthContext"
 import { router } from "./router"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient();
 
 function App() {
 
 
   return (
     <AuthContextProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </AuthContextProvider>
   )
 }
