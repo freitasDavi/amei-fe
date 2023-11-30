b1dc0a0724bedbbda1ac455ca1d1792de949c0a0
Davi <davi.frrs@gmail.com>
2023-11-30 10:06:35 -0300

    a

--- src/reports/Agendamentos.tsx
+++ src/reports/Agendamentos.tsx
@@ -1,5 +1,5 @@
 import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts"
+import * as pdfFonts from "pdfmake/build/vfs_fonts"
 // import * as pdfFonts from '@/lib/pdf/vfs_fonts';
 import { TDocumentDefinitions } from "pdfmake/interfaces";
 import { format } from "date-fns";
