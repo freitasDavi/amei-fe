5875011efaf8bb574e5b695e1f433fb5b7ac25ff
Davi <davi.frrs@outlook.com>
2023-12-01 20:02:50 -0300

    Ajustes da landing page

--- src/components/Home/Footer.tsx
+++ src/components/Home/Footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
                 <a href="#" className="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white">
                     <img src="/assets/LOGO-COMPLETA-AZUL.png" className="mr-3 h-6 sm:h-9" alt="Logo amei" />
                 </a>
-                <p className="my-6 text-gray-500 dark:text-gray-400">Open-source library of over 400+ web components and interactive elements built for better web.</p>
+                <p className="my-6 text-gray-500 dark:text-gray-400"></p>
                 <ul className="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
                     <li>
                         <a href="#home" className="mr-4 hover:underline md:mr-6 ">Início</a>
@@ -33,7 +33,7 @@ export function Footer() {
                         <a href="#" className="mr-4 hover:underline md:mr-6">Contact</a>
                     </li> */}
                 </ul>
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" className="hover:underline">a-mei™ - tkn™</a>. Todos direitos reservados.</span>
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" className="hover:underline">a-mei™ - tkn</a>. Todos direitos reservados.</span>
             </div>
         </footer>
     )
--- src/components/Home/PriceCard.tsx
+++ src/components/Home/PriceCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 type Props = {
     title: string
@@ -46,9 +47,9 @@ export function PriceCard({ title, price, description, setPlano }: Props) {
                     Começar
                 </button>
             ) : (
-                <a href="#" className="text-white bg-primary-logo hover:bg-primary-logo-dark focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">
+                <Link to="/register" className="text-white bg-primary-logo hover:bg-primary-logo-dark focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900">
                     Começar
-                </a>
+                </Link>
             )}
         </div>
     )
